4fcf91cd7ae672fc74340c20ccded7734e99ef56
Hazrat Ali <iloveyii@yahoo.com>
2017-03-30 00:17:39 +0200

    Added cross domain cors behavior

--- frontend/controllers/PostsController.php
+++ frontend/controllers/PostsController.php
@@ -2,9 +2,29 @@
 namespace frontend\controllers;
 
 use yii\rest\ActiveController;
-
+use yii\filters\Cors;
 
 class PostsController extends  ActiveController
 {
+    public $enableCsrfValidation = false;
     public $modelClass = 'frontend\models\Post';
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return array_merge(parent::behaviors(), [
+            'corsFilter'  => [
+                'class' => Cors::className(),
+                'cors'  => [
+                    'Origin'                           => ['*'],
+                    'Access-Control-Request-Method'    => ['*'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age'           => 3600,
+                ],
+            ],
+
+        ]);
+    }
+
 }
