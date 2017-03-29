3184afbe4402a014ea28692c2b407d09c4acb2d1
Hazrat Ali <iloveyii@yahoo.com>
2017-03-29 20:26:40 +0200

    added post model and crud

---
+++ frontend/models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $body
+ * @property integer $userId
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['userId'], 'integer'],
+            [['title'], 'string', 'max' => 50],
+            [['body'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'body' => 'Body',
+            'userId' => 'User ID',
+        ];
+    }
+}
